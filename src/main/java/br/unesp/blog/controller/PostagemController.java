87e29c398a78ec90c6e8eb890fb47e0ce9f6c65c
Kayke Guilherme Vertu <vertu_kayke@hotmail.com>
2026-05-20 21:20:22 -0300

    Adaptado código para comportar os componentes

--- src/main/java/br/unesp/blog/controller/PostagemController.java
+++ src/main/java/br/unesp/blog/controller/PostagemController.java
@@ -1,15 +1,19 @@
 package br.unesp.blog.controller;
 
 import br.unesp.blog.entity.Blog;
+import br.unesp.blog.entity.Comentario;
+import br.unesp.blog.entity.Componente;
 import br.unesp.blog.entity.Postagem;
 import br.unesp.blog.repository.BlogRepository;
 import br.unesp.blog.repository.PostagemRepository;
 import java.util.List;
 import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +24,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller("PostagemController")
 @RequestMapping("/postagem")
-// TODO: Implementar Novas mudanças envolvendo componentes
+@Transactional
+@Slf4j
 public class PostagemController {
 
     @Autowired
@@ -29,20 +34,27 @@ public class PostagemController {
     @Autowired
     private BlogRepository blogRepository;
 
-    @GetMapping("/")
-    public ResponseEntity<List<Postagem>> listarPostagens() {
+    @GetMapping("/{id}")
+    public ResponseEntity<List<Postagem>> listarPostagens(
+        @PathVariable Long id
+    ) {
         List<Postagem> postagens = (List<
             Postagem
-        >) postagemRepository.findAll();
+        >) postagemRepository.findByBlogId(id);
 
         return new ResponseEntity<>(postagens, HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Postagem> listarPostagem(@PathVariable Long id) {
-        Optional<Postagem> postagem = postagemRepository.findById(id);
+    @GetMapping("/{idBlog}/{idPost}")
+    public ResponseEntity<Postagem> listarPostagem(
+        @PathVariable Long idBlog,
+        @PathVariable Long idPost
+    ) {
+        Optional<Postagem> postagem = postagemRepository.findById(idPost);
 
-        if (postagem.isPresent()) {
+        if (
+            postagem.isPresent() && postagem.get().getBlog().getId() == idBlog
+        ) {
             return new ResponseEntity<>(postagem.get(), HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -54,6 +66,20 @@ public class PostagemController {
         @RequestBody Postagem entity
     ) {
         resolverBlog(entity);
+
+        // Sincroniza o lado inverso da relação
+        // Evita violação de chave estrangeira
+        if (entity.getConteudo() != null) {
+            for (Componente componente : entity.getConteudo()) {
+                componente.setPostagem(entity);
+            }
+        }
+        if (entity.getComentarios() != null) {
+            for (Comentario comentario : entity.getComentarios()) {
+                comentario.setPostagem(entity);
+            }
+        }
+
         Postagem savedPostagem = postagemRepository.save(entity);
 
         return new ResponseEntity<>(savedPostagem, HttpStatus.OK);
@@ -63,7 +89,26 @@ public class PostagemController {
     public ResponseEntity<Postagem> atualizarPostagem(
         @RequestBody Postagem entity
     ) {
+        Optional<Postagem> p = postagemRepository.findById(entity.getId());
+        if (p.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        entity.setBlog(p.get().getBlog());
         resolverBlog(entity);
+
+        // Sincroniza o lado inverso da relação
+        // Evita violação de chave estrangeira
+        if (entity.getConteudo() != null) {
+            for (Componente componente : entity.getConteudo()) {
+                componente.setPostagem(entity);
+            }
+        }
+        if (entity.getComentarios() != null) {
+            for (Comentario comentario : entity.getComentarios()) {
+                comentario.setPostagem(entity);
+            }
+        }
+
         Postagem updatedPostagem = postagemRepository.save(entity);
 
         return new ResponseEntity<>(updatedPostagem, HttpStatus.OK);
