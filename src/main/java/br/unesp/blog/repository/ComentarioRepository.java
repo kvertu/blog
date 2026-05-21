90821b5fbde3d45d3084517c678f5acdadbc6b74
Kayke Guilherme Vertu <vertu_kayke@hotmail.com>
2026-05-20 21:17:26 -0300

    Adicionado método para buscar blog por id Removido espaços vazios

--- src/main/java/br/unesp/blog/repository/ComentarioRepository.java
+++ src/main/java/br/unesp/blog/repository/ComentarioRepository.java
@@ -1,11 +1,9 @@
 package br.unesp.blog.repository;
 
+import br.unesp.blog.entity.Comentario;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import br.unesp.blog.entity.Comentario;
-
 @Repository
-public interface ComentarioRepository extends CrudRepository<Comentario, Long> {
-    
-}
+public interface ComentarioRepository
+    extends CrudRepository<Comentario, Long> {}
--- src/main/java/br/unesp/blog/repository/PostagemRepository.java
+++ src/main/java/br/unesp/blog/repository/PostagemRepository.java
@@ -1,11 +1,11 @@
 package br.unesp.blog.repository;
 
+import br.unesp.blog.entity.Postagem;
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import br.unesp.blog.entity.Postagem;
-
 @Repository
 public interface PostagemRepository extends CrudRepository<Postagem, Long> {
-    
+    List<Postagem> findByBlogId(Long id);
 }
